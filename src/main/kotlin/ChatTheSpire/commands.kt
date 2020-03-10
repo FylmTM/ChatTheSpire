bf1a71c72d9df69eb74c0366e1367d5f242a3b72
Dmitry Vrublevsky <dmitry@vrublevsky.me>
2020-03-10 21:14:35 +0200

    Destroy potion command

--- src/main/kotlin/ChatTheSpire/commands.kt
+++ src/main/kotlin/ChatTheSpire/commands.kt
@@ -1,6 +1,7 @@
 package ChatTheSpire
 
 import ChatTheSpire.console.CardConsoleCommand
+import ChatTheSpire.console.PotionDestroyConsoleCommand
 import ChatTheSpire.console.PotionUseConsoleCommand
 import ChatTheSpire.console.TestConsoleCommand
 import basemod.devcommands.ConsoleCommand
@@ -9,4 +10,5 @@ fun initializeCommands() {
     ConsoleCommand.addCommand(":test", TestConsoleCommand::class.java)
     ConsoleCommand.addCommand(":card", CardConsoleCommand::class.java)
     ConsoleCommand.addCommand(":potionuse", PotionUseConsoleCommand::class.java)
+    ConsoleCommand.addCommand(":potiondestroy", PotionDestroyConsoleCommand::class.java)
 }
---
+++ src/main/kotlin/ChatTheSpire/console/PotionDestroyConsoleCommand.kt
@@ -0,0 +1,41 @@
+package ChatTheSpire.console
+
+import ChatTheSpire.command.PotionDestroyCommand
+import ChatTheSpire.command.PotionUseCommand
+import ChatTheSpire.util.Spire
+import ChatTheSpire.util.toSafeArrayList
+import basemod.DevConsole
+import basemod.devcommands.ConsoleCommand
+import java.util.ArrayList
+
+class PotionDestroyConsoleCommand : ConsoleCommand() {
+
+    init {
+        minExtraTokens = 1
+        maxExtraTokens = 1
+    }
+
+    override fun execute(tokens: Array<String>, depth: Int) {
+        val parameters = tokens.drop(1).map(String::toInt)
+        if (!PotionDestroyCommand.perform(parameters)) {
+            DevConsole.log("Failed to destroy potion.")
+        }
+    }
+
+    override fun extraOptions(tokens: Array<String>, depth: Int): ArrayList<String> {
+        if (tokens.size == 2) {
+            return Spire.potions
+                ?.mapIndexed() { i, potion -> "${i + 1} :: ${potion.name}" }
+                .toSafeArrayList()
+        }
+        if (tokens.size > 2) {
+            tooManyTokensError()
+        }
+        return ArrayList()
+    }
+
+    override fun errorMsg() {
+        DevConsole.couldNotParse()
+        DevConsole.log("syntax: :potiondestroy [potion-position] {monster-position}")
+    }
+}
